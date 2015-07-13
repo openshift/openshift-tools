323b6a7c6061174f22a8b34d263a37b06d5e864f
Thomas Wiest <twiest@redhat.com>
2015-07-13 15:55:00 -0400

    added ops-zagg-processor.py

---
+++ openshift_tools/ansible/simplezabbix.py
@@ -0,0 +1,359 @@
+# vim: expandtab:tabstop=4:shiftwidth=4
+
+#
+#   Copyright 2015 Red Hat Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+"""simplezabbix Module
+The purpose of this module is to give a simple interface into zabbix using the
+Ansible Runner and the openshift-ansible zbxapi module.
+"""
+
+import ansible.runner
+import json
+
+class ResultsException(Exception):
+    """Used when the results of an operation aren't what was expect
+    """
+    pass
+
+class SimpleZabbixRaw(object):
+    """A raw interface into the zbxapi and ansible runner calls.
+
+    The purpose of this module is to be a lower level api. It returns exactly
+    what ansible returns (no processing done, no evaluations made).
+    """
+
+    def __init__(self, url, user, password):
+        """Contructs the object
+
+        Args:
+            url: the zabbix api URL (ex: http://localhost/zabbix/api_jsonrpc.php)
+            user: the zabbix api user
+            password: the zabbix api password
+        """
+        self.url = url
+        self.user = user
+        self.password = password
+
+        # for now, we want to always run the zabbix module locally
+        self.pattern = 'localhost'
+
+    def get_template_info(self, templates):
+        """Queries Zabbix to get template information.
+
+        Args:
+            templates: a list of template names to get information for.
+
+        Returns:
+            The raw ansible results dictionary.
+
+            Example:
+            {
+                "dark": {},
+                "contacted": {
+                    "localhost": {
+                        "invocation": {
+                            "module_name": "zbxapi",
+                            "module_args": ""
+                        },
+                        "state": "list",
+                        "changed": false,
+                        "results": [
+                            {
+                                [... SNIP ...]
+                                "host": "Template Heartbeat",
+                                "templateid": "10085",
+                                [... SNIP ...]
+                            },
+                            {
+                                [... SNIP ...]
+                                "host": "Template Host",
+                                "templateid": "10088",
+                                [... SNIP ...]
+                            }
+                        ]
+                    }
+                }
+            }
+        """
+        args = {
+            'server': self.url,
+            'user': self.user,
+            'password': self.password,
+            'zbx_class': 'Template',
+            'state': 'list',
+            'params': {
+                'filter': {
+                    'host': templates,
+                }
+            }
+        }
+
+        results = self._run_ansible(args)
+        return results
+
+    def get_hostgroup_info(self, hostgroups):
+        """Queries Zabbix to get hostgroup information.
+
+        Args:
+            hostgroups: a list of hostgroups to get information for.
+
+        Returns:
+            The raw ansible results dictionary.
+
+            Example:
+            {
+                "dark": {},
+                "contacted": {
+                    "localhost": {
+                        "invocation": {
+                            "module_name": "zbxapi",
+                            "module_args": ""
+                        },
+                        "state": "list",
+                        "changed": false,
+                        "results": [
+                            {
+                                "internal": "0",
+                                "flags": "0",
+                                "groupid": "2",
+                                "name": "Linux servers"
+                            },
+                            {
+                                "internal": "0",
+                                "flags": "0",
+                                "groupid": "6",
+                                "name": "Virtual machines"
+                            }
+                        ]
+                    }
+                }
+            }
+        """
+        args = {
+            'server': self.url,
+            'user': self.user,
+            'password': self.password,
+            'zbx_class': 'Hostgroup',
+            'state': 'list',
+            'params': {
+                'filter': {
+                    'name': hostgroups,
+                }
+            }
+        }
+
+        results = self._run_ansible(args)
+        return results
+
+    def ensure_host_is_present(self, name, tids, hgids, interfaces=None):
+        """Ensures a host entry is present in zabbix.
+
+        Args:
+            name: the name of the host in zabbix
+            tids: a list of template ids
+            hgids: a list of hostgroup ids
+            interfaces: optional interfaces definition
+
+        Returns:
+            The raw ansible results dictionary.
+
+            Example:
+            {
+                "dark": {},
+                "contacted": {
+                    "localhost": {
+                        "invocation": {
+                            "module_name": "zbxapi",
+                            "module_args": ""
+                        },
+                        "state": "present",
+                        "changed": true,
+                        "results": {
+                            "jsonrpc": "2.0",
+                            "result": {
+                                "hostids": [
+                                    "10112"
+                                ]
+                            },
+                            "id": 1
+                        }
+                    }
+                }
+            }
+        """
+        if not interfaces:
+            interfaces = [
+                {
+                    'type': 1,
+                    'main': 1,
+                    'useip': 1,
+                    'ip': '127.0.0.1',
+                    'dns': '',
+                    'port': 10050,
+                }
+            ]
+
+        args = {
+            'server': self.url,
+            'user': self.user,
+            'password': self.password,
+            'zbx_class': 'Host',
+            'state': 'present',
+            'params': {
+                'host': name,
+                'interfaces': interfaces,
+                'groups': hgids,
+                'templates': tids,
+                'output': 'extend',
+                'filter': {
+                    'host': [name],
+                },
+                'selectGroups': ['groupids'],
+                'selectParentTemplates': ['templateids'],
+            },
+            'ignore': ['interfaces'],
+        }
+
+        results = self._run_ansible(args)
+        return results
+
+    def _run_ansible(self, args):
+        """Actually make the call to the ansible runner."""
+        results = ansible.runner.Runner(
+            forks=1,
+            pattern=self.pattern,
+            transport='local',
+            module_name='zbxapi',
+            complex_args=args,
+        ).run()
+
+        if not results:
+            raise ResultsException("Ansible module run failed, no results given.")
+
+        if not results['contacted']:
+            message = "Ansible module run failed: module output:\n%s" % \
+                      json.dumps(results, indent=4)
+            raise ResultsException(message)
+
+        return results
+
+class SimpleZabbix(object):
+    """A simple interface into the zbxapi and ansible runner calls.
+
+    The purpose of this module is to be a higher level api. It returns
+    simple data in normal python data structures. It's meant to address the
+    90% simple cases. For the other 10% cases, use SimpleZabbixRaw or the
+    Ansible runner interface directly.
+    """
+    def __init__(self, url, user, password):
+        """Contructs the object
+
+        Args:
+            url: the zabbix api URL (ex: http://localhost/zabbix/api_jsonrpc.php)
+            user: the zabbix api user
+            password: the zabbix api password
+        """
+        self.raw = SimpleZabbixRaw(url, user, password)
+
+    def get_template_ids(self, templates):
+        """Queries Zabbix to get template IDs.
+
+        Args:
+            templates: a list of templates to get IDs for.
+
+        Returns:
+            A list of template IDs.
+
+            Example: [u'10085', u'10088']
+        """
+        results = self.raw.get_template_info(templates)
+        ids = [r['templateid'] for r in results['contacted'][self.raw.pattern]['results']]
+        return ids
+
+    def get_hostgroup_ids(self, hostgroups):
+        """Queries Zabbix to get hostgroup IDs.
+
+        Args:
+            hostgroups: a list of hostgroups to get IDs for.
+
+        Returns:
+            A list of hostgroup IDs.
+
+            Example: [u'2', u'6']
+        """
+        results = self.raw.get_hostgroup_info(hostgroups)
+        ids = [r['groupid'] for r in results['contacted'][self.raw.pattern]['results']]
+        print ids
+        return ids
+
+    def ensure_host_is_present(self, name, templates, hostgroups):
+        """Ensures a host entry is present in zabbix.
+
+        Note, this is an idempotent operation:
+            if the host is present, and the same, nothing will be done
+            if the host is present, but different, an attempt will be made to update it
+            if the host is not present, it will be created.
+
+        Args:
+            name: the name of the host in zabbix
+            templates: a list of templates the is linked to
+            hostgroups: a list of hostgroups the host belongs to
+
+        Returns:
+            The a boolean:
+                True: the host is present and configured.
+                False: an error occurred.
+
+            Example:
+            {
+                "dark": {},
+                "contacted": {
+                    "localhost": {
+                        "invocation": {
+                            "module_name": "zbxapi",
+                            "module_args": ""
+                        },
+                        "state": "present",
+                        "changed": true,
+                        "results": {
+                            "jsonrpc": "2.0",
+                            "result": {
+                                "hostids": [
+                                    "10112"
+                                ]
+                            },
+                            "id": 1
+                        }
+                    }
+                }
+            }
+        """
+        tids = self.get_template_ids(templates)
+        hgids = self.get_hostgroup_ids(hostgroups)
+
+        result = self.raw.ensure_host_is_present(name, tids, hgids)
+
+        if result['contacted'][self.raw.pattern]['results'].has_key('result') and \
+           result['contacted'][self.raw.pattern]['results']['result']['hostids'][0]:
+            return True # we got a hostid back, so we're all good
+
+
+        if result['contacted'][self.raw.pattern]['results'].has_key('hostids') and \
+            result['contacted'][self.raw.pattern]['results']['hostids'][0]:
+            return True # we got a hostid back, so we're all good
+
+        return False # something went wrong
---
+++ openshift_tools/monitoring/zabbix_metric_processor.py
@@ -0,0 +1,137 @@
+# vim: expandtab:tabstop=4:shiftwidth=4
+
+#
+#   Copyright 2015 Red Hat Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+"""zabbix_metric_processor Module
+The purpose of this module is to process metrics and send them to Zabbix.
+"""
+
+from openshift_tools.monitoring.metricmanager import UniqueMetric
+from zbxsend import send_to_zabbix
+
+# Reason: disable pylint too-few-public-methods because this class in a simple
+#     helper / wrapper class.
+# Status: permanently disabled
+# pylint: disable=too-few-public-methods
+class ZabbixSender(object):
+    """Used as a wrapper to bind together the authentication and the send call.
+    """
+    def __init__(self, server, port):
+        """Constructs the object
+
+        Args:
+            server: the zabbix server where the trapper is running
+            port: the zabbix port that the trapper is listening on
+        """
+        self.server = server
+        self.port = port
+
+    def send(self, metrics):
+        """Sends the metric information to the zabbix trapper.
+
+        Args:
+            metrics: a list of UniqueMetrics to send to zabbix.
+
+        Returns: a boolean
+            True: metrics were successfully sent to zabbix.
+            False: an error occurred.
+        """
+        return send_to_zabbix(metrics, self.server, self.port)
+
+class ZabbixMetricProcessor(object):
+    """Processes metrics and sends them to Zabbix Trapper.
+    """
+
+    def __init__(self, metric_manager, zbxapi, zbxsender):
+        """Constructs the object
+
+        Args:
+            metric_manager: this is where we get the metrics from.
+            zbxapi: this is used for heartbeat metrics
+            zbxsender: this is used to send the metrics to zabbix
+        """
+        self.metric_manager = metric_manager
+        self.zbxapi = zbxapi
+        self.zbxsender = zbxsender
+
+    def process_metrics(self):
+        """Processes all metrics provided by metric_manager"""
+        # Read metrics from disk
+        all_metrics = self.metric_manager.read_metrics()
+
+        # Process heartbeat metrics First (this ordering is important)
+        # This ensures a host in zabbix has been created.
+        self._process_heartbeat_metrics(self.metric_manager.filter_heartbeat_metrics(all_metrics))
+
+        # Now process normal metrics
+        self._process_normal_metrics(self.metric_manager.filter_zbx_metrics(all_metrics))
+
+    def _process_heartbeat_metrics(self, hb_metrics):
+        """Processes heartbeat metrics.
+
+        This ensures that there is a host entry in zabbix, then
+        sends a value for the heartbeat item.
+
+        Args:
+            hb_metrics: a list of heartbeat metrics to process.
+
+        Returns: nothing
+        """
+        for hb_metric in hb_metrics:
+            templates = hb_metric.value['templates']
+            hostgroups = hb_metric.value['hostgroups']
+
+            # Make sure there is a host entry in zabbix
+            host_res = self.zbxapi.ensure_host_is_present(
+                hb_metric.host,
+                templates,
+                hostgroups)
+
+            # Actually do the heartbeat now
+            hbum = UniqueMetric(hb_metric.host, 'heartbeat.ping', 1)
+            hb_res = self.zbxsender.send([hbum])
+
+            # Remove the metric if we were able to successfully heartbeat
+            if host_res and hb_res:
+                # We've successfuly sent the heartbeat, so remove it from disk
+                self.metric_manager.remove_metrics(hb_metric)
+            else:
+                # TODO: add logging of the failure, and signal of failure
+                # For now, we'll just leave them on disk and try again
+                pass
+
+    def _process_normal_metrics(self, metrics):
+        """Processes normal metrics.
+
+        This sends the metric data to the zabbix trapper.
+        sends a value for the heartbeat item.
+
+        Args:
+            metrics: a list of metrics to send to zabbix.
+
+        Returns: nothing
+        """
+        if not metrics:
+            return True # we successfully sent 0 metrics to zabbix
+
+        if self.zbxsender.send(metrics):
+            # We've successfuly sent the metrics, so remove them from disk
+            self.metric_manager.remove_metrics(metrics)
+        else:
+            # TODO: add logging of the failure, and signal of failure
+            # For now, we'll just leave them on disk and try again
+            pass
