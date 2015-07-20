ca3b398002f5bd47cfc251dcb630f19e72cb4102
Thomas Wiest <twiest@redhat.com>
2015-07-20 16:35:25 -0400

    an attempt at syncing up kwoodson's zabbix-server container definitions with the main repo

--- docker/oso-rhel7-zabbix-server/start.sh
+++ docker/oso-rhel7-zabbix-server/start.sh
@@ -1,13 +1,45 @@
 #!/bin/bash -e
 
+echo user:x:$(id -u):0:USER:/root:/bin/bash >> /etc/passwd
+
+sed -ri -e "s/^DBUser=.*/DBUser=${MYSQL_USER}/g" \
+        -e "s/^DBPassword=.*/DBPassword=${MYSQL_PASSWORD}/g" \
+        -e "s/^DBHOst=.*/DBHost=${MYSQL_HOST}/g" /etc/zabbix/zabbix_server.conf
+
+
 echo
 echo 'Ensure database exists.'
 echo '---------------'
 /root/zabbix/createdb.sh
 echo
 
+# Clean up the extra templates
 echo
-echo 'Starting crond'
+echo 'Clean up zabbix templates'
 echo '---------------'
-/usr/sbin/crond -n
+ansible-playbook /root/ansible/playbooks/clean_zabbix.yml
 echo
+
+# Create the heartbeat and os linux templates along with items
+echo
+echo 'Create zabbix templates'
+echo '---------------'
+ansible-playbook /root/ansible/playbooks/setup_zabbix.yml
+echo
+
+echo
+echo 'Starting zabbix agent'
+echo '---------------'
+/usr/sbin/zabbix_agentd -c /etc/zabbix/zabbix_agentd.conf
+echo
+
+echo
+echo 'Starting zabbix'
+echo '---------------'
+/usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
+echo
+
+while true; do
+  sleep 5
+done
+
