24c7c869c1383015f5b7bb3721221317ab12ab30
Kenny Woodson <kwoodson@redhat.com>
2015-07-21 16:42:05 -0400

    Updated zabbix server to work on v3

--- docker/oso-rhel7-zabbix-server/zabbix/db_create/createdb.sh
+++ docker/oso-rhel7-zabbix-server/zabbix/db_create/createdb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-/usr/bin/mysqlshow -u${MYSQL_USER} -h ${MYSQL_HOST} ${MYSQL_DATABASE} &>/dev/null
+/usr/bin/mysqlshow -u${MYSQL_USER} -h${MYSQL_HOST} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} | grep -q trigger_discovery
+
 
 if [ "$?" == 1 ]; then
-  /usr/bin/mysql -uroot -h ${MYSQL_HOST} < /root/zabbix/create_zabbix.sql
-  /usr/bin/mysql -uroot -h ${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/schema.sql
-  /usr/bin/mysql -uroot -h ${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/images.sql
-  /usr/bin/mysql -uroot -h ${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/data.sql
+  /usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} < /root/zabbix/create_zabbix.sql
+  /usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/schema.sql
+  /usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/images.sql
+  /usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/data.sql
 else
   echo "Database ${MYSQL_DATABASE} exists.  Skipping setup."
 fi
