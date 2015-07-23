2f8dddd7be408052d2663064c067eb4b9041923a
Kenny Woodson <kwoodson@redhat.com>
2015-07-23 12:07:08 -0400

    Fixed db create

--- docker/oso-rhel7-zabbix-server/zabbix/db_create/createdb.sh
+++ docker/oso-rhel7-zabbix-server/zabbix/db_create/createdb.sh
@@ -4,7 +4,7 @@
 
 
 if [ "$?" == 1 ]; then
-  /usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} < /root/zabbix/create_zabbix.sql
+  #/usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} < /root/zabbix/create_zabbix.sql
   /usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/schema.sql
   /usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/images.sql
   /usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -h${MYSQL_HOST} zabbix < /usr/share/doc/zabbix-server-mysql-2.4.5/create/data.sql
