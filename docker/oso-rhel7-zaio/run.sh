337900310ddd67ecb5aaacb5889e6045b86a4b4d
Thomas Wiest <twiest@redhat.com>
2015-07-29 09:46:05 -0400

    removed ansible zabbix config from zaio. Instead, use the latest from openshift-ansible (this makes it operate like a normal zabbix instance, and ensures we're using the latest configs).

--- docker/oso-rhel7-zaio/run.sh
+++ docker/oso-rhel7-zaio/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
 echo -n "Running zaio... "
-docker run -p 10050:10050 -p 10051:10051 -p 80:80 -p 443:443 --name oso-rhel7-zaio oso-rhel7-zaio
-echo "Done.
+docker run -ti --rm -p 10050:10050 -p 10051:10051 -p 80:80 -p 443:443 --name oso-rhel7-zaio oso-rhel7-zaio
