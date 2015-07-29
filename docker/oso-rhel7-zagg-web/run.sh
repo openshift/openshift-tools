68819c6ad67876d5ee27d88c6970d98fed63985c
Thomas Wiest <twiest@redhat.com>
2015-07-29 13:51:08 -0400

    changed oso-rhel7-zag-web run script to default to joining the oso-rhel7-zaio network namespace (makes dev much easier)

--- docker/oso-rhel7-zagg-web/run.sh
+++ docker/oso-rhel7-zagg-web/run.sh
@@ -25,8 +25,9 @@
 export ZAGG_SERVER_CONFIG='{ "targets": [ { "name": "local cluster zbx server", "type": "zabbix", "trapper_server": "localhost", "trapper_port": 10051, "api_url": "http://localhost/zabbix/api_jsonrpc.php", "api_user": "Admin", "api_pass": "zabbix", "path": "/var/run/zagg/data/cluster-zbx", } ] }'
 
 
-if [ $# -gt 0 ] ; then
-  docker run -e "ZAGG_SERVER_CONFIG=$ZAGG_SERVER_CONFIG" -ti --rm --name zagg-web --privileged -e NAME=zagg-web -e IMAGE=zagg-web  -v /etc/localtime:/etc/localtime -v /var/lib/docker/volumes/shared/:/shared:rw -p 8000:80 zagg-web $1
-else
-  docker run -e "ZAGG_SERVER_CONFIG=$ZAGG_SERVER_CONFIG" -d --name zagg-web --privileged -e NAME=zagg-web -e IMAGE=zagg-web  -v /etc/localtime:/etc/localtime -v /var/lib/docker/volumes/shared/:/shared:rw -p 8000:80 zagg-web
-fi
+docker run --net container:oso-rhel7-zaio -ti --rm --name oso-rhel7-zagg-web \
+  -e "ZAGG_SERVER_CONFIG=$ZAGG_SERVER_CONFIG" \
+  -e NAME=zagg-web -e IMAGE=zagg-web \
+  -v /etc/localtime:/etc/localtime -v /var/lib/docker/volumes/shared/:/shared:rw \
+  -p 8000:80 \
+  oso-rhel7-zagg-web $1
