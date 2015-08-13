a733daab829a4472c5940e6a09489aeba521458c
Thomas Wiest <twiest@redhat.com>
2015-08-13 10:30:07 -0400

    Added restrictions so that you can only push from a RHEL box.

--- docker/oso-rhel7-zagg-client/push.sh
+++ docker/oso-rhel7-zagg-client/push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! grep -qi 'Red Hat Enterprise Linux' /etc/redhat-release ; then
+  echo "ERROR: We only allow pushing from a RHEL machine because it allows secrets volumes."
+  exit 1
+fi
+
 # Make sure base is pushed with the latest changes since we depend on it.
 if ../oso-rhel7-ops-base/push.sh ; then
   # Push ourselves
