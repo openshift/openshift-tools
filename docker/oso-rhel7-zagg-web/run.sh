4306f101b1be87626115dbf1b0c287aa340cf09f
Thomas Wiest <twiest@redhat.com>
2015-08-13 13:46:48 -0400

    fixed the docker run scripts so that they all link together by default.

--- docker/oso-rhel7-zagg-web/run.sh
+++ docker/oso-rhel7-zagg-web/run.sh
@@ -8,36 +8,31 @@
 #{
 #  "targets": [
 #    {
-#      "name": "local cluster zabbix server",
+#      "name": "local zaio",
 #      "type": "zabbix",
-#      "trapper_server": "localhost",
+#      "trapper_server": "oso-rhel7-zaio",
 #      "trapper_port": 10051,
-#      "api_url": "http://localhost/zabbix/api_jsonrpc.php",
+#      "api_url": "http://oso-rhel7-zaio/zabbix/api_jsonrpc.php",
 #      "api_user": "Admin",
 #      "api_password": "zabbix",
-#      "path": "/var/run/zagg/data/local_cluster_zbx",
+#      "path": "/var/run/zagg/data/local_zaio",
 #    },
-#    {
-#      "name": "remote zagg",
-#      "type": "zagg",
-#      "host": "localhost:8000",
-#      "user": "admin",
-#      "password": "password",
-#      "path": "/var/run/zagg/data/remote_zagg",
-#    }
 #  ]
 #}
 #
 
 
-export ZAGG_SERVER_CONFIG='{ "targets": [ { "name": "local cluster zabbix server", "type": "zabbix", "trapper_server": "localhost", "trapper_port": 10051, "api_url": "http://localhost/zabbix/api_jsonrpc.php", "api_user": "Admin", "api_password": "zabbix", "path": "/var/run/zagg/data/local_cluster_zbx", }, { "name": "remote zagg", "type": "zagg", "host": "localhost:8000", "user": "admin", "password": "password", "path": "/var/run/zagg/data/remote_zagg", } ] }'
+export ZAGG_SERVER_CONFIG='{ "targets": [ { "name": "local zaio", "type": "zabbix", "trapper_server": "oso-rhel7-zaio", "trapper_port": 10051, "api_url": "http://oso-rhel7-zaio/zabbix/api_jsonrpc.php", "api_user": "Admin", "api_password": "zabbix", "path": "/var/run/zagg/data/local_zaio", }, ] }'
+
+sudo echo -e "\nTesting sudo works...\n"
 
   #--net container:oso-rhel7-zaio \
-docker run -ti --rm --name oso-rhel7-zagg-web \
+sudo docker run -ti --rm --name oso-rhel7-zagg-web \
+  --link oso-rhel7-zaio:oso-rhel7-zaio \
   -e "ZAGG_SERVER_CONFIG=$ZAGG_SERVER_CONFIG" \
   -e "ZAGG_SERVER_USER=admin" \
   -e "ZAGG_SERVER_PASSWORD=password" \
-  -e NAME=zagg-web -e IMAGE=zagg-web \
-  -v /etc/localtime:/etc/localtime -v /var/lib/docker/volumes/shared/:/shared:rw \
-  -p 8000:80 \
+  -v /etc/localtime:/etc/localtime \
+  -v /var/lib/docker/volumes/shared:/shared:rw \
+  -p 8000:8000 \
   oso-rhel7-zagg-web $1
--- docker/oso-rhel7-zaio/run.sh
+++ docker/oso-rhel7-zaio/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
+sudo echo -e "\nTesting sudo works...\n"
+
 echo -n "Running zaio... "
-docker run -ti --rm -p 10050:10050 -p 10051:10051 -p 80:80 -p 443:443 --name oso-rhel7-zaio oso-rhel7-zaio
+sudo docker run -ti --rm -p 10050:10050 -p 10051:10051 -p 80:80 -p 443:443 --name oso-rhel7-zaio oso-rhel7-zaio
