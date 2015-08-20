a07ea71ac8b07cbb6fc8e8585effe0342e38c225
Thomas Wiest <twiest@redhat.com>
2015-08-20 11:36:46 -0400

    changed oso-rhel7-zagg-client to send a heartbeat when the container starts.

--- docker/oso-rhel7-zagg-client/start.sh
+++ docker/oso-rhel7-zagg-client/start.sh
@@ -3,6 +3,9 @@
 # Configure the container
 time ansible-playbook /root/config.yml
 
+# Send a heartbeat when the container starts up
+/usr/bin/ops-zagg-client --send-heartbeat
+
 # Run the main service of this container
 echo
 echo 'Starting crond'
