bd06afd975f95678a931dcc9cce38a668f5bf08b
Kenny Woodson <kwoodson@redhat.com>
2015-08-31 15:59:09 -0400

    Adding host type vars to zagg-client

--- docker/oso-rhel7-zagg-client/run.sh
+++ docker/oso-rhel7-zagg-client/run.sh
@@ -2,6 +2,15 @@
 
 sudo docker run --rm=true -it --name oso-rhel7-zagg-client \
            --net=container:oso-f22-host-monitoring \
+           -e ZAGG_SERVER=localhost                                             \
+           -e ZAGG_USER=admin                                 \
+           -e ZAGG_PASSWORD=zagg_server                         \
+           -e ZAGG_CLIENT_HOSTNAME=localhost                                    \
+           -e OSO_CLUSTER_GROUP=localhost                              \
+           -e OSO_CLUSTER_ID=localhost                                   \
+           -e OSO_HOST_TYPE=master      \
+           -e OSO_SUB_HOST_TYPE=default                              \
+           -v /etc/localtime:/etc/localtime                                              \
+           -v /run/pcp:/run/pcp                                                          \
            -v /var/lib/docker/volumes/shared:/shared:rwZ \
-           -v /run/pcp:/run/pcp \
            oso-rhel7-zagg-client $@
