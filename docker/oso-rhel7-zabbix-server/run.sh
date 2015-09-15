68dfe73241215d181a516daffd6036c03743207f
Thomas Wiest <twiest@redhat.com>
2015-09-15 16:53:30 -0400

    fixes to make the latest oso-rhel7-zabbix-server work in openshift

--- docker/oso-rhel7-zabbix-server/run.sh
+++ docker/oso-rhel7-zabbix-server/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
 echo -n "Running zabbix server..."
-sudo docker run -ti --net=host -p 10050:10050 -p 10051:10051 oso-rhel7-zabbix-server $@
+sudo docker run -ti \
+       --net=host \
+       -p 10050:10050 \
+       -p 10051:10051 \
+       -v /var/lib/docker/volumes/shared:/shared:rw \
+     oso-rhel7-zabbix-server $@
 echo "Done."
