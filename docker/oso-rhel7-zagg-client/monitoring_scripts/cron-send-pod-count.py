87262cf29c3485aab52644456470597ace5766ba
Sten Turpin <sten@redhat.com>
2015-10-08 08:49:51 -0500

    should pass pylint now

--- docker/oso-rhel7-zagg-client/monitoring_scripts/cron-send-pod-count.py
+++ docker/oso-rhel7-zagg-client/monitoring_scripts/cron-send-pod-count.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
+'''
+   Command to send count of running pods to Zabbix
+'''
+
+#This is not a module, but pylint thinks it is.  This is a command.
+#pylint: disable=invalid-name
+
 import subprocess
 from openshift_tools.monitoring.zagg_sender import ZaggSender
 
-## get list of running pods 
 
-podlist_cmd = "KUBECONFIG=/etc/openshift/master/admin.kubeconfig /usr/bin/oadm manage-node --list-pods --selector=''"
+def main():
+    ''' Get data from oadm and send to zabbix
+    '''
 
-# get the output of oadm
-output = subprocess.check_output(podlist_cmd, shell=True)
+    ## get list of running pods
+    podlist_cmd = "oadma manage-node --list-pods --selector=''"
 
-# pare down to only lines that contain "Running" 
-running_pods_list = [p for p in output.split('\n') if "Running" in p]
+    # get the output of oadm
+    output = subprocess.check_output(podlist_cmd, shell=True)
 
-# we now have all the data we want.  Let's send it to Zagg
+    # pare down to only lines that contain "Running"
+    running_pods_list = [p for p in output.split('\n') if "Running" in p]
 
-zs = ZaggSender()
+    # we now have all the data we want.  Let's send it to Zagg
+    zs = ZaggSender()
+    zs.add_zabbix_keys({'running_pods_count' : len(running_pods_list)})
 
-zs.add_zabbix_keys({ 'running_pods_count' : len(running_pods_list )})
+    # Finally, sent them to zabbix
+    zs.send_metrics()
 
-# Finally, sent them to zabbix
-zs.send_metrics()
+if __name__ == '__main__':
+    main()
