0f83ee619feb015c44c9253d8c1ab26adf9d6af1
Joel Diaz <jdiaz@redhat.com>
2015-11-02 15:07:42 -0500

    add scripts to check and report pcp ping state update zagg-client playbooks to add cron job for checking pcp ping state

---
+++ scripts/monitoring/cron-send-pcp-ping.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 0: problem
+# 1: all is well
+PING_RETURN=1
+
+# Check whether pcp appears to be alive and well
+pminfo -f kernel.uname.sysname | grep -i "linux"
+
+if [ $? -ne 0 ] ; then
+	echo "pcp reporting bogus data"
+	PING_RETURN=0
+fi
+
+# Send results to zabbix
+ops-zagg-client -k "pcp.ping" -o "$PING_RETURN"
