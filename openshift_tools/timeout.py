aeda3b17ae60484ebb552fb8d9e1747907f8f340
Joel Diaz <jdiaz@redhat.com>
2015-11-13 14:03:09 -0500

    Docker cron timing
    create dokerutil library
    bring cron-send-docker-metrics.py into openshift-tools-scripts
    removing timing function from cron-send-docker-metrics.py
    create cron-send-docker-timer.py to provide timing info

--- openshift_tools/timeout.py
+++ openshift_tools/timeout.py
@@ -1,50 +1,77 @@
 # vim: expandtab:tabstop=4:shiftwidth=4
 
-''' The purpose of this module is to give a decorator that can be used to
-    timeout a function if it runs for too long.
+''' The purpose of this module is to give a decorator and anonymous block
+    statement that can be used to timeout a function or block of code if
+    it runs for too long.
 
     Example Usage:
-        from openshift_tools.timeout import timeout
+        from openshift_tools.timeout import timeout, timed
 
-        @timeout(20)
+        @timed(20)
         def some_function():
             # do something that should always finish within 20 seconds
+
+        def some_function():
+            with timeout(seconds=20):
+                # do something that should always finish within 20 seconds
 '''
 
 import signal
 from functools import wraps
 
 class TimeoutException(Exception):
-    ''' Class to hold timeout exceptions '''
+    ''' For raising timeouts '''
     pass
 
+def timed(seconds):
+    ''' Use as a decorator to timeout a function after so many seconds '''
 
-def handler(signum, frame):
-    ''' Handles the alarm signal '''
-    del signum # To make pylint happy
-    del frame  # To make pylint happy
-    raise TimeoutException("Operation Timed Out")
-
-def timeout(seconds):
-    ''' Use as a decorator to timeout a function after so many seconds
-    '''
     def outer(decorated_function):
-        ''' Outer function of the decorator
-        '''
+        ''' Outer function of the decorator '''
+
         @wraps(decorated_function)
         def wrapper(*args, **kwargs):
-            ''' Wraps the decorated function, where the magic happens
-            '''
-            old_handler = signal.signal(signal.SIGALRM, handler)
-            signal.alarm(seconds)
-            try:
-                result = decorated_function(*args, **kwargs)
-            finally:
-                # Reset the alarm
-                signal.alarm(0)
+            ''' Wraps the decorated function, where the magic happens '''
 
-                # make sure the original handler is now back in place
-                signal.signal(signal.SIGALRM, old_handler)
+            with timeout(seconds=seconds):
+                result = decorated_function(*args, **kwargs)
+
             return result
         return wrapper
     return outer
+
+# Reason: disable pylint invalid-name
+#         pylint does not like the name 'timeout'
+# Status: permanently disabled
+# pylint: disable=invalid-name
+class timeout(object):
+    ''' anonymous block for setting arbritrary timeouts '''
+
+    # Reason: disable pylint too-few-public-methods
+    # Status: permanently disabled
+    # pylint: disable=too-few-public-methods
+    def __init__(self, seconds, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+        self.old_handler = None
+
+    def handle_timeout(self, signum, frame):
+        ''' called in the event of a timeout. just raise an exception '''
+
+        del signum #make pylint happy
+        del frame  #make pylint happy
+        raise TimeoutException(self.error_message)
+
+    def __enter__(self):
+        self.old_handler = signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+
+    # Reason: disable pylint redefined-builtin
+    # Status: permanently disabled
+    # pylint: disable=redefined-builtin
+    def __exit__(self, type, value, traceback):
+        # Reset the alarm
+        signal.alarm(0)
+
+        # make sure the original handler is now back in place
+        signal.signal(signal.SIGALRM, self.old_handler)
---
+++ scripts/monitoring/cron-send-docker-timer.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+# vim: expandtab:tabstop=4:shiftwidth=4
+
+'''
+    Report time to execute 'docker info'
+'''
+
+# Adding the ignore because it does not like the naming of the script
+# to be different than the class name
+# pylint: disable=invalid-name
+
+
+from docker import AutoVersionClient
+from docker.errors import DockerException
+from openshift_tools.monitoring.zagg_sender import ZaggSender
+from openshift_tools.timeout import TimeoutException
+from openshift_tools.monitoring.dockerutil import DockerUtil
+import json
+import time
+
+if __name__ == "__main__":
+    keys = None
+    zs = ZaggSender()
+    try:
+        cli = AutoVersionClient(base_url='unix://var/run/docker.sock')
+        du = DockerUtil(cli, max_wait=360)
+
+        # Wait up to 6 minutes
+        time_start = time.time()
+        du.get_disk_usage()
+        time_stop = time.time()
+        elapsed_ms = int((time_stop - time_start) * 1000)
+
+        keys = {
+            'docker.info_elapsed_ms': elapsed_ms
+        }
+    except (DockerException, TimeoutException) as ex:
+        print "\nERROR talking to docker: %s\n" % ex.message
+        keys = {
+            'docker.info_elapsed_ms': 360000 # 360000 = 6 minutes
+        }
+
+    zs.add_zabbix_keys(keys)
+
+    print "Sending these metrics:"
+    print json.dumps(keys, indent=4)
+    zs.send_metrics()
+    print "\nDone.\n"
