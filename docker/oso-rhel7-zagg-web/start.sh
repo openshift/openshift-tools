cc083189b37ac9027b54afee24b7340348e1fdfc
Thomas Wiest <twiest@redhat.com>
2015-12-03 16:57:22 -0500

    added the ability to pause indefinitely on startup to all of our rhel7 containers.

--- docker/oso-rhel7-zagg-web/start.sh
+++ docker/oso-rhel7-zagg-web/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
+# This is useful so we can debug containers running inside of OpenShift that are
+# failing to start properly.
+if [ "$OO_PAUSE_ON_START" = "true" ] ; then
+  echo
+  echo "This container's startup has been paused indefinitely because OO_PAUSE_ON_START has been set."
+  echo
+  while true ; do
+    sleep 10
+  done
+fi
+
+
 echo user:x:$(id -u):0:USER:/root:/bin/bash >> /etc/passwd
 
 # Configure the container on startup
