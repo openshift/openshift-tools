37b16c350ae27069fa4afcafd5c788c44d388bdf
Joel Diaz <jdiaz@redhat.com>
2016-03-02 16:54:41 -0500

    split zbxapi into its own subpackage

--- docker/oso-rhel7-zagg-web/library/zbx_action.py
+++ docker/oso-rhel7-zagg-web/library/zbx_action.py
@@ -28,7 +28,7 @@
 # pylint: disable=duplicate-code
 
 # pylint: disable=import-error
-from openshift_tools.monitoring.zbxapi import ZabbixAPI, ZabbixConnection, ZabbixAPIError
+from openshift_tools.zbxapi import ZabbixAPI, ZabbixConnection, ZabbixAPIError
 
 def exists(content, key='result'):
     ''' Check if key exists in content or the size of content[key] > 0
