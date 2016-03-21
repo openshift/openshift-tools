1471448cda0dcf63d7cc07e87c256c74f91b99d6
zhizhang <zhizhang@zhizhang-laptop-nay.redhat.com>
2016-03-21 09:04:36 +0800

    fix some pyint rules

--- scripts/monitoring/cron-send-pvspace-sum.py
+++ scripts/monitoring/cron-send-pvspace-sum.py
@@ -9,12 +9,8 @@
 # the to-many-branches
 
 import subprocess
-import json
-import time
-import re
-import urllib
 #import sys
-import os
+#import os
 
 # Our jenkins server does not include these rpms.
 # In the future we might move this to a container where these
@@ -23,7 +19,7 @@ import os
 from openshift_tools.monitoring.zagg_sender import ZaggSender
 
 
-def oc_cmd(cmd, verbose=False):
+def oc_cmd(cmd):
     '''Base command for oc
     '''
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, \
@@ -40,12 +36,10 @@ def main():
     ''' get the pvspace sum
     '''
 
-    sum = oc_cmd("oc get pv |awk '{print $3}'|sed 's/Gi//g'|awk '{s+=$1} END {print s}'")
-    print 'the total of pv space is : %s' % sum
+    sum_pv = oc_cmd("oc get pv |awk '{print $3}'|sed 's/Gi//g'|awk '{s+=$1} END {print s}'")
+    print 'the total of pv space is : %s' % sum_pv
     zgs = ZaggSender()
-    zgs.add_zabbix_keys({'openshift.master.pv.space.total': sum})
-   # zgs.add_zabbix_keys({'openshift.master.app.create.time': CreateTime})
-   # zgs.add_zabbix_keys({'openshift.master.app.build.time': BuildTime})
+    zgs.add_zabbix_keys({'openshift.master.pv.space.total': sum_pv})
     zgs.send_metrics()
 
 
