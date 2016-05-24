14dab6f44764884d5d2f95c582d4ee467034032f
zhizhang <zhizhang@zhizhang-laptop-nay.redhat.com>
2016-05-24 15:26:13 +0800

    add openshift.master.api cpu and mem check

--- scripts/monitoring/cron-send-cpu-mem-process.sh
+++ scripts/monitoring/cron-send-cpu-mem-process.sh
@@ -13,3 +13,24 @@ echo "the mem usage of openshift.node process is $MemUsagenode"
 
 ops-zagg-client -k "openshift.nodeprocess.cpu" -o "$CpuUsagenode"
 ops-zagg-client -k "openshift.nodeprocess.mem" -o "$MemUsagenode"
+
+#the cpu use of master api process
+
+
+processidofmaster=`ps -ef |grep '/usr/bin/openshift start master api --config=/etc/origin/master/master-config.yaml'|grep -v "grep" |awk '{print $2}'`
+
+if [ "$processidofmaster" != "" ];
+then
+    CpuUsagemasterapi=`ps -p $processidofmaster -o %cpu |grep -v CPU`
+
+    echo "the cpu usage of openshift.master.api is $CpuUsagemasterapi"
+
+    MemUsagemasterapi=`ps -p $processidofmaster -o %mem |grep -v MEM`
+
+    echo "the mem usage of openshift.master.api is $MemUsagemasterapi"
+
+    ops-zagg-client -k "openshift.master.api.cpu" -o "$CpuUsagenode"
+    ops-zagg-client -k "openshift.master.api.mem" -o "$MemUsagenode"
+else
+    echo 
+fi
