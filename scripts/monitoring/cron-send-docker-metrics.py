873735dcfb2c73538244fb10fcddecf4c0fa780e
zhizhang <zhizhang@zhizhang-laptop-nay.redhat.com>
2016-05-30 10:31:26 +0800

    add auto-heal for the docker storage

--- scripts/monitoring/cron-send-docker-metrics.py
+++ scripts/monitoring/cron-send-docker-metrics.py
@@ -11,6 +11,7 @@
 
 
 from docker import AutoVersionClient
+from docker import Client
 from docker.errors import DockerException
 from openshift_tools.monitoring.zagg_sender import ZaggSender
 from openshift_tools.timeout import TimeoutException
@@ -22,9 +23,11 @@ if __name__ == "__main__":
     zs = ZaggSender()
     try:
         cli = AutoVersionClient(base_url='unix://var/run/docker.sock')
+        cli_storage = Client(base_url='unix://var/run/docker.sock')
         du = DockerUtil(cli)
+        Deadcontainers = cli_storage.containers(quiet=True,filters={'status':'exited'}) #get all the exited container
         du_dds = du.get_disk_usage()
-
+        print "data_space:",du_dds.data_space_percent_available
         keys = {
             'docker.storage.data.space.used': du_dds.data_space_used,
             'docker.storage.data.space.available': du_dds.data_space_available,
@@ -46,8 +49,35 @@ if __name__ == "__main__":
         }
 
     zs.add_zabbix_keys(keys)
-
     print "Sending these metrics:"
     print json.dumps(keys, indent=4)
     zs.send_metrics()
     print "\nDone.\n"
+    
+    #start auto-heal 
+
+    #Deadcontainer = cli_storage.containers(filters={'id':'bf34a7c35b907c1f3047321b3073aa74f16957c2505af3644c2c1e7bb976bd78'})
+    #print '******************************8'
+    #print Deadcontainer[0]['Status']
+    print '***********************************'
+    if int(du_dds.data_space_percent_available) < 50:
+        print 'less than 50'
+        #print Deadcontainers
+        #remote those dead contaners
+        #check the status before remote(double check)
+        for container in Deadcontainers:
+            print container['Id']
+            #get the info
+            Deadcontainer = cli_storage.containers(all=True,filters={'id':container['Id']})
+            #print Deadcontainer
+            if len(Deadcontainer) == 1:
+                print '******************************'
+                print Deadcontainer[0]['Status']
+                print '***********************************'
+                if Deadcontainer[0]['Status'].find('Exited') != -1:
+                    #do the remove step
+                    print Deadcontainer[0]['Status'].find('Exited')
+            else:
+                print 'container not exist'
+    else:
+        print 'not less',int(du_dds.data_space_percent_available)
