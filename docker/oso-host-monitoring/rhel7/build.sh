3e2c37a3a12aa1a370a9edff70c72948acaafd0b
Drew Anderson <dranders@redhat.com>
2016-07-20 10:01:31 +1000

    run generate-containers.yml

--- docker/oso-host-monitoring/rhel7/build.sh
+++ docker/oso-host-monitoring/rhel7/build.sh
@@ -14,6 +14,10 @@ sudo echo -e "\nTesting sudo works...\n"
 # We MUST be in the same directory as this script for the build to work properly
 cd $(dirname $0)
 
+# generate container fingerprint (user/location/timestamp/git information)
+container_fingerprint='./container-build-env-fingerprint.output'
+./container-build-env-fingerprint.sh > ${container_fingerprint}
+
 ## Make sure base is built with latest changes since we depend on it.
 # commenting this out for now because ops-base does a full rebuild everytime
 #if ../oso-rhel7-ops-base/build.sh ; then
@@ -23,3 +27,6 @@ cd $(dirname $0)
   sudo time docker build $@ -t oso-rhel7-host-monitoring . && \
   sudo docker tag -f oso-rhel7-host-monitoring docker-registry.ops.rhcloud.com/ops/oso-rhel7-host-monitoring
 #fi
+
+# cleanup
+rm -f ${container_fingerprint}
