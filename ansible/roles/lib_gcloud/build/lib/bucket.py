a96c6e24a809b7d7ab76e9e11367116bf4edc8c3
Kenny Woodson <kwoodson@redhat.com>
2016-07-22 14:37:46 -0400

    Adding storage buckets to dm

---
+++ ansible/roles/lib_gcloud/build/lib/bucket.py
@@ -0,0 +1,20 @@
+# pylint: skip-file
+
+# pylint: disable=too-many-instance-attributes
+class Bucket(GCPResource):
+    '''Object to represent a gcp storage bucket'''
+
+    resource_type = "storage.v1.bucket"
+
+    # pylint: disable=too-many-arguments
+    def __init__(self, rname, project, zone):
+        '''constructor for gcp resource'''
+        super(Bucket, self).__init__(rname, Bucket.resource_type, project, zone)
+
+    def to_resource(self):
+        """ return the resource representation"""
+        return {'name': self.name + '-bucket',
+                'type': Bucket.resource_type,
+                'properties': {'name': self.name}
+               }
+
