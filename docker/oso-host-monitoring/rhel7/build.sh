d9458e5339574fdee94dfa70acbe8615c59f3067
Ivan Horvath <ihorvath@redhat.com>
2016-07-26 15:38:09 -0400

    removing push from osohm/rhel7 and adding tag

--- docker/oso-host-monitoring/rhel7/build.sh
+++ docker/oso-host-monitoring/rhel7/build.sh
@@ -20,6 +20,12 @@ cd $(dirname $0)
   # Build ourselves
   echo
   echo "Building oso-rhel7-host-monitoring..."
+  TAG=''
+  GITBRANCH=$(git name-rev --name-only HEAD)
+  if [ "${GITBRANCH}" != prod ]
+  then
+    TAG=":${GITBRANCH}"
+  fi
   sudo time docker build $@ -t oso-rhel7-host-monitoring . && \
-  sudo docker tag -f oso-rhel7-host-monitoring docker-registry.ops.rhcloud.com/ops/oso-rhel7-host-monitoring
+  sudo docker tag -f oso-rhel7-host-monitoring docker-registry.ops.rhcloud.com/ops/oso-rhel7-host-monitoring"${TAG}"
 #fi
