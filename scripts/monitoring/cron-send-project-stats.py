e309b552cd7303a87292817d18978605d9c1f215
zhizhang <zhizhang@zhizhang-laptop-nay.redhat.com>
2016-10-20 10:58:41 +0800

    fix bug for Terminating project last for -1 days

--- scripts/monitoring/cron-send-project-stats.py
+++ scripts/monitoring/cron-send-project-stats.py
@@ -253,9 +253,11 @@ def main():
                 current_time = datetime.datetime.now()
                 time_keeps = current_time - old_time
                 print 'the project in Terminating status for %s' % time_keeps
-
-                time_keeps_max = max(time_keeps_max, (current_time - old_time).seconds)
-
+                if current_time > old_time:
+                    time_keeps_max = max(time_keeps_max, (current_time - old_time).seconds)
+                else:
+                    print 'something wrong , the pod said its been terminating before created'
+                    #time_keeps_max = -((current_time - old_time).seconds)
                 print time_keeps_max
 
     except ValueError, e:
