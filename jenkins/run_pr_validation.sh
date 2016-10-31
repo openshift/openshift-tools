176e7a515c3475712282f2e4ce51f315097c4e17
Timothy Williams <tiwillia@redhat.com>
2016-10-31 16:36:24 -0400

    Initial dockerfile test

---
+++ jenkins/run_pr_validation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# TODO
+# Handle failure scenarios
+
+GIT_COMMIT=$1
+# The below were previously extracted using the jenkins pull request builder plugin
+ghprbTargetBranch=$2
+ghprbActualCommit=$3
+
+yaml_validation.sh "$(/usr/bin/git rev-parse HEAD^)" "$GIT_COMMIT" "$ghprbTargetBranch"
+pylint.sh $(/usr/bin/git rev-parse HEAD^) $GIT_COMMIT $ghprbTargetBranch
+parent.py $ghprbTargetBranch $ghprbActualCommit
