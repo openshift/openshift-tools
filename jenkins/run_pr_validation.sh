55278a1a9a56125d20e36dd4f695dde4f7982cfb
Timothy Williams <tiwillia@redhat.com>
2016-11-07 14:38:45 -0500

    ignore me

--- jenkins/run_pr_validation.sh
+++ jenkins/run_pr_validation.sh
@@ -3,12 +3,19 @@
 # TODO
 # Handle failure scenarios
 
-GIT_COMMIT=$1
-# The below were previously extracted using the jenkins pull request builder plugin
-ghprbTargetBranch=$2
-ghprbActualCommit=$3
+TOOLS_BRANCH=$1
+REMOTE=$2
+REMOTE_BRANCH=$3
+GIT_COMMIT=$4
 
-echo $(pwd)
-./yaml_validation.sh "$(/usr/bin/git rev-parse HEAD^)" "$GIT_COMMIT" "$ghprbTargetBranch"
-./pylint.sh $(/usr/bin/git rev-parse HEAD^) $GIT_COMMIT $ghprbTargetBranch
-./parent.py $ghprbTargetBranch $ghprbActualCommit
+pushd $HOME/openshift-tools
+git checkout $TOOLS_BRANCH
+git fetch origin $TOOLS_BRANCH
+git remote add target https://github.com/$REMOTE
+git fetch target $REMOTE_BRANCH
+git merge target/$REMOTE_BRANCH
+
+# There is no reason for the below script to require 3 inputs, it just throws away the third. But it still requires 3 inputs ¯\_(ツ)_/¯
+./yaml_validation.sh "$(/usr/bin/git rev-parse HEAD^)" "$GIT_COMMIT" "bullshit"
+./pylint.sh $(/usr/bin/git rev-parse HEAD^) $GIT_COMMIT $TOOLS_BRANCH
+./parent.py $TOOLS_BRANCH $GIT_COMMIT
