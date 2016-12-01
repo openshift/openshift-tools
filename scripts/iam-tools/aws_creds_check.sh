40f3fbf8d494ca13f215810c301a223a62c563bd
Joel Smith <joesmith@redhat.com>
2016-12-01 12:23:56 -0500

    Add aws_creds_check to openshift-tools-scripts-iam-tools RPM
    Also add ASL 2.0 licence file to repo root and ASL 2.0 header
    to iam-tools scripts

--- scripts/iam-tools/aws_creds_check.sh
+++ scripts/iam-tools/aws_creds_check.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#   Copyright 2016 Red Hat Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 # Meant to be sourced by login shells
 # Checks to see if the user logging in has credentials for all AWS accounts
 # mentioned in /etc/openshift_tools/aws_accounts.txt and prompts them to create
