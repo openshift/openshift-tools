63c6973aba21b157458a6dd66259943e330a91d8
Joel Smith <joesmith@redhat.com>
2016-12-05 23:38:12 -0500

    SSO monitoring: fix heartbeat/check race with short initial sleep

--- docker/oso-monitor-sso/centos7/start.sh
+++ docker/oso-monitor-sso/centos7/start.sh
@@ -27,10 +27,16 @@ ts='{ print strftime("%Y-%m-%d %H:%M:%S"), $0; fflush(); }'
 /usr/local/bin/ops-run-in-loop $((60*5)) ops-runner -f -s 60 -n sso.monitoring.send.heartbeat.quick /usr/bin/ops-zagg-client  -k heartbeat.ping -o 1 2>&1 | awk "$ts" &
 
 echo
-echo 'Running SSO functionality check every 24 hours'
-echo '----------------'
-/usr/local/bin/ops-run-in-loop 86400 ops-runner -f -s 15 -n check.sso.iam.status /usr/local/bin/check_sso_service 2>&1 | awk "$ts" | tee /var/log/monitor-sso.log &
-echo
 echo 'Running container and HTTP status check every 5 minutes'
 echo '----------------'
-/usr/local/bin/ops-run-in-loop 300 ops-runner -f -s 15 -n check.sso.container.status /usr/local/bin/check_sso_http_status 2>&1 | awk "$ts" | tee /var/log/monitor-sso.log
+/usr/local/bin/ops-run-in-loop 300 ops-runner -f -s 15 -n check.sso.container.status /usr/local/bin/check_sso_http_status 2>&1 | awk "$ts" | tee /var/log/monitor-sso.log &
+echo
+# we seem to have a race condition where the above heartbeat that creates the host sometimes hasn't been processed by the time the check below gets processed so the
+# data from the first send is lost. While it's not crucial that we get the data, it's nice not to have empty items, so we add a small delay in the hopes that
+# most of the time, the heartbeat will get processed first.
+echo Sleeping for 5 minutes before sending first SSO functionality check
+sleep $((5*60))
+echo
+echo 'Running SSO functionality check every 24 hours'
+echo '----------------'
+/usr/local/bin/ops-run-in-loop 86400 ops-runner -f -s 15 -n check.sso.iam.status /usr/local/bin/check_sso_service 2>&1 | awk "$ts" | tee /var/log/monitor-sso.log
