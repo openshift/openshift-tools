a2a099d60074aa76fae572393db8f8d85327e8a7
Blair Morrison <bmorriso@redhat.com>
2017-01-18 15:57:31 -0500

    Fixing linting issues

--- scripts/monitoring/cron-send-stuck-builds.py
+++ scripts/monitoring/cron-send-stuck-builds.py
@@ -38,8 +38,9 @@ def parse_args():
     argparser = argparse.ArgumentParser(description='OpenShift stuck new build count')
     argparser.add_argument('-v', '--verbose', action='store_true', default=None, help='Verbose?')
     argparser.add_argument('-a', '--age', action='store', default=180, type=int,
-                           help='How long (in seconds) a build can stay in the state before being considered stuck. Default: 180')
-    argparser.add_argument('-s', '--state', action='store', default='New', help='The build state being checked. Default: New')
+                           help='Seconds that a build can be in the state before its considered stuck. Default: 180')
+    argparser.add_argument('-s', '--state', action='store', default='New',
+                           help='The build state being checked. Default: New')
     return argparser.parse_args()
 
 def send_metrics(stuck_builds, build_state):
