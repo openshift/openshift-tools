9e3a472a17ee067788182597d902ffaf6d8a4163
Matt Woodson <mwoodson@redhat.com>
2017-08-01 14:07:25 -0400

    updated verify-cicd-operation

--- scripts/cicd/verify-cicd-operation.py
+++ scripts/cicd/verify-cicd-operation.py
@@ -5,6 +5,7 @@
 # pylint: disable=superfluous-parens
 # pylint: disable=logging-not-lazy
 # pylint: disable=bare-except
+# pylint: disable=star-args
 
 #Jenkins:
 #ssh use-tower1.ops.rhcloud.com <operation> [approved_arg1] [approved_arg2] ...
@@ -25,9 +26,7 @@ import socket
 import logging
 import logging.handlers
 
-
-
-
+program_to_run = "/home/opsmedic/aos-cd/git/aos-cd-jobs/tower-scripts/bin/cicd-control.sh"
 valid_operations = ['build-ci-msg',
                     'commit-config-loop',
                     'delete',
@@ -39,6 +38,7 @@ valid_operations = ['build-ci-msg',
                     'enable-zabbix-maint',
                     'generate-byo-inventory',
                     'install',
+                    'legacy-upgrade',
                     'perf1',
                     'perf2',
                     'perf3',
@@ -55,13 +55,22 @@ valid_operations = ['build-ci-msg',
                     'upgrade-nodes',
                    ]
 
+def build_arg_list(clusterid, operation, *args):
+    ''' build a list of args '''
+
+    arg_list = ['-c', clusterid, '-o', operation]
+
+    for arg in args:
+        arg_list += ['-a', arg]
+
+    return arg_list
+
 def runner(program, *args):
     ''' run the script that is intended '''
     try:
         os.execlp(program, program, *args)
     except:
         pass
-    print("exec failed for command: " + str.join(" ", [program] + list(args)))
     sys.exit(11)
     # runner never returns
 
@@ -92,7 +101,6 @@ if len(cmd_args) == 0:
 
 op = cmd_args.pop(0)  # Remove operation string from argument list and store
 
-#match = re.match(r"(?P<operation>install|delete|upgrade|status|logs|perf1|perf2|perf3|build-ci-msg|smoketest)", op)
 valid_operations_string = '|'.join(valid_operations)
 match = re.match(r"(?P<operation>" + valid_operations_string + ")", op)
 
@@ -105,10 +113,13 @@ if not match:
 # Validate remaining arguments against strict patterns.
 # Only matched patters may be passed on to cicd-control. See approved list
 # at the top of this file.
+# TODO: This needs to be streamlined
 operation_args = []
 for s in cmd_args:
-    if re.match("(^docker-version=[a-zA-Z0-9._-]+$)", s):  # Example docker version: docker-1.12.6-30.git97ba2c0.el7
-        operation_args.append("--" + s)
+    if re.match("(^docker_version=[a-zA-Z0-9._-]+$)", s):  # Example docker version: docker-1.12.6-30.git97ba2c0.el7
+        operation_args.append(s)
+    elif re.match("(^openshift_ansible_build=[a-zA-Z0-9./-]+$)", s):  # Example: 3.6.126.11/1.git.0.bd61c80.el7
+        operation_args.append(s)
     else:
         logger.info("%s Restricted key '%s' disallowed argument: %s" % (os.path.basename(__file__), cluster_id, s))
         print("Argument doesn't match an allowed pattern")
@@ -117,7 +128,5 @@ for s in cmd_args:
 
 logger.info("%s Restricted key '%s' running command: %s" % (os.path.basename(__file__), cluster_id, cmd))
 
-runner("/home/opsmedic/aos-cd/git/aos-cd-jobs/tower-scripts/bin/cicd-control.sh",
-       cluster_id,
-       match.group("operation"),
-       *operation_args)
+args_to_send = build_arg_list(cluster_id, match.group('operation'), *operation_args)
+runner(program_to_run, *args_to_send)
