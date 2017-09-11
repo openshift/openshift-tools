ddc5dbb497e4f0aacf15de791dbc262e3f4cfd6a
Matt Woodson <mwoodson@redhat.com>
2017-09-11 10:51:11 -0400

    made some modifications to ohi and awsutil to accomodate the cluster var

--- openshift_tools/inventory_clients/awsutil.py
+++ openshift_tools/inventory_clients/awsutil.py
@@ -72,7 +72,7 @@ class AwsUtil(object):
 
         tags = []
         inv = self.get_inventory()
-        for key in inv.keys():
+        for key in inv:
             matched = regex.match(key)
             if matched:
                 tags.append(matched.group(1))
@@ -259,9 +259,10 @@ class AwsUtil(object):
         inv = self.get_inventory()
         variables = []
         for host in inv['oo_clusterid_' + cluster]:
-            variables.append(inv['_meta']['hostvars'][host][variable])
+            if variable in inv['_meta']['hostvars'][host]:
+                variables.append(inv['_meta']['hostvars'][host][variable])
 
         if len(list(set(variables))) == 1:
             return variables[0]
-        else:
-            return "Unable to determine cluster variable: %s" %variable
+
+        return None
