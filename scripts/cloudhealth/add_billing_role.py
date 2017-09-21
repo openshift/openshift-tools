d740ac8c3a8b9b50d3038084bb468c3bfa80ecf3
Brett Lentz <blentz@redhat.com>
2017-09-21 11:14:15 -0400

    add cloudhealth scripts

---
+++ scripts/cloudhealth/add_billing_role.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+# Copyright 2017 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.""
+
+'''
+This script adds a role to AWS accounts to enable access from cloudhealth
+'''
+
+from ConfigParser import ConfigParser
+
+import argparse
+import json
+import logging
+import os
+import sys
+
+import boto3
+import botocore.exceptions
+import requests.exceptions
+
+from cloudhealth import CloudHealthAwsAccount
+
+LOG_FORMAT = '%(asctime)-15s [%(levelname)s] (%(name)s.%(funcName)s) %(message)s'
+AWS_CREDENTIALS = os.environ['HOME'] + '/.aws/credentials'
+
+def setup_logging(level=logging.NOTSET):
+    ''' configure logging'''
+    logging.basicConfig(format=LOG_FORMAT, level=level)
+    log = logging.getLogger(__name__)
+    return log
+
+def parse_args():
+    ''' parse CLI args '''
+    parser = argparse.ArgumentParser(description='MY SCRIPT DESCRIPTION')
+    parser.add_argument('-e', '--external-id', type=str, dest='aws_external_id',
+                        help="External ID String for AWS cross-account trust")
+    parser.add_argument('-f', '--file', type=str, dest='credsfile',
+                        default='%s/.aws/credentials' % os.environ['HOME'],
+                        help='aws credentials file')
+    parser.add_argument('-k', '--cloudhealth-key', type=str, dest='cht_api_key',
+                        help="The API Key for CloudHealth")
+    parser.add_argument('-n', '--name', type=str, dest='aws_name',
+                        default='default',
+                        help="The name used for the AWS Role and Policy. Must match permissions file name, too.")
+    parser.add_argument('-p', '--profile', type=str,
+                        help="The profile name for the AWS account")
+    parser.add_argument('-r', '--region', type=str, default='us-east-1',
+                        help='AWS region, default: us-east-1')
+    parser.add_argument('-v', action='count', dest='verbose',
+                        help='verbosity. up to -vvv supported.')
+
+    args = parser.parse_args()
+    return args
+
+def get_account_number():
+    ''' get AWS Account number from STS '''
+    session = boto3.Session(profile_name=os.environ['AWS_PROFILE'])
+    sts = session.client('sts')
+    query = sts.get_caller_identity()
+    return query['Account']
+
+def setup_role_policy(account_number, name='default'):
+    ''' create the role and policy in AWS '''
+
+    role_policy_document = load_json('%s.json' % name)
+    policy_document = load_json('%s.json' % name)
+    policy_arn = 'arn:aws:iam::%s:policy/%s' % (account_number, name)
+
+    session = boto3.Session(profile_name=os.environ['AWS_PROFILE'])
+    iam = session.client('iam')
+
+    check_role = None
+    try:
+        check_role = iam.get_role(RoleName=name)
+        LOG.info(check_role)
+    except botocore.exceptions.ClientError as exc:
+        LOG.debug(exc.message)
+
+    if not check_role:
+        # pylint: disable=line-too-long
+        resp = iam.create_role(RoleName=name,
+                               AssumeRolePolicyDocument=json.dumps(role_policy_document))
+        LOG.info(resp)
+        LOG.warn('AWS Role created.')
+    else:
+        LOG.warn('AWS Role found. Skipping creation.')
+
+    check_policy = None
+    try:
+        check_policy = iam.get_policy(PolicyArn=policy_arn)
+        LOG.info(check_policy)
+    except botocore.exceptions.ClientError as exc:
+        LOG.debug(exc.message)
+
+    if not check_policy:
+        resp = iam.create_policy(PolicyName=name,
+                                 PolicyDocument=json.dumps(policy_document))
+        LOG.info(resp)
+        LOG.warn('AWS Policy created.')
+    else:
+        LOG.warn('AWS Policy found. Skipping creation.')
+
+    check_role_policy = None
+    try:
+        check_role_policy = iam.get_role_policy(RoleName=name,
+                                                PolicyName=name)
+        LOG.info(check_role_policy)
+    except botocore.exceptions.ClientError as exc:
+        LOG.debug(exc.message)
+
+    if not check_role_policy:
+        resp = iam.attach_role_policy(RoleName=name, PolicyArn=policy_arn)
+        LOG.info(resp)
+        LOG.warn('AWS Role Policy created.')
+    else:
+        LOG.warn('AWS Role Policy found. Skipping creation.')
+
+def setup_cloudhealth_account(api_key, account_number, external_id=None):
+    ''' create an account listing in cloudhealth '''
+    cht = CloudHealthAwsAccount(api_key=api_key,
+                                owner_id=u'%s' % str(account_number))
+    LOG.info('CloudHealth Account: %s', cht)
+    # pylint: disable=line-too-long
+    if not getattr(cht, 'id', None):
+        cht.authentication = {'assume_role_external_id': external_id,
+                              'protocol': 'assume_role',
+                              'assume_role_arn': 'arn:aws:iam::%s:role/oso-billing' % \
+                                    str(account_number)}
+        cht.name = "%s (%s)" % (os.environ['AWS_PROFILE'], account_number)
+        try:
+            resp = cht.create()
+            LOG.debug('CHT Response: %s', resp)
+            LOG.warn('CloudHealth Account created.')
+        except requests.exceptions.HTTPError as exc:
+            LOG.error('CloudHealth Account create FAILED: %s', exc.message)
+    elif cht.authentication['assume_role_external_id'] != external_id:
+        cht.authentication = {'assume_role_external_id': external_id,
+                              'protocol': 'assume_role',
+                              'assume_role_arn': 'arn:aws:iam::%s:role/oso-billing' % \
+                                    str(account_number)}
+        try:
+            resp = cht.update()
+            LOG.debug('CHT Response: %s', resp)
+            LOG.warn('CloudHealth Account updated.')
+        except requests.exceptions.HTTPError as exc:
+            LOG.error('CloudHealth Account update FAILED: %s', exc.message)
+    else:
+        LOG.warn('CloudHealth Account already exists. Skipping creation.')
+
+def load_json(filename):
+    ''' load json file '''
+    loadedjson = None
+    try:
+        with open(filename) as openfile:
+            loadedjson = json.load(openfile)
+    except TypeError:
+        loadedjson = json.load(filename)
+    except IOError:
+        loadedjson = json.loads('{}')
+    except ValueError:
+        LOG.error('Unable to read %s', filename)
+    return loadedjson
+
+def main():
+    ''' main function '''
+
+    # pylint: disable=line-too-long
+    if ARGS.cht_api_key:
+        api_key = ARGS.cht_api_key
+    elif os.environ.get('CLOUDHEALTH_API_KEY', None):
+        api_key = os.environ.get('CLOUDHEALTH_API_KEY', None)
+    else:
+        LOG.error('CloudHealth API Key missing. Use -k or set CLOUDHEALTH_API_KEY in your environment.')
+        sys.exit(1)
+
+    if ARGS.profile:
+        if not os.environ.get('AWS_PROFILE', None):
+            LOG.warn('AWS_PROFILE environment variable not set. Trying profile value.')
+            os.environ['AWS_PROFILE'] = ARGS.profile
+        account_number = get_account_number()
+        LOG.info('using AWS Account: %s', account_number)
+        setup_role_policy(account_number, name=ARGS.name)
+        setup_cloudhealth_account(api_key, account_number)
+    else:
+        LOG.warn('No profile specified. Looping through all profiles in credentials file.')
+        awscreds = ConfigParser()
+        awscreds.read(ARGS.credsfile)
+        for sect in awscreds.sections():
+            LOG.warn('refreshing profile: %s', sect)
+            os.environ['AWS_PROFILE'] = sect
+            account_number = get_account_number()
+            LOG.info('using AWS Account: %s', account_number)
+            setup_role_policy(account_number, name=ARGS.name)
+            setup_cloudhealth_account(api_key, account_number)
+
+if '__main__' in __name__:
+    ARGS = parse_args()
+
+    LEVEL = logging.ERROR
+    if ARGS.verbose == 1:
+        LEVEL = logging.WARN
+    elif ARGS.verbose == 2:
+        LEVEL = logging.INFO
+    elif ARGS.verbose >= 3:
+        LEVEL = logging.DEBUG
+
+    LOG = setup_logging(level=LEVEL)
+    try:
+        LOG.debug('CLI opts: %s', ARGS)
+        main()
+    except KeyboardInterrupt:
+        LOG.warn("Ctrl-C caught. Exiting...")
+        sys.exit(0)
