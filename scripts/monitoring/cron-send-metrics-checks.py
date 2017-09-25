58235e239af258287bc04fec5a70451e2a2e1750
Dan Yocum <dyocum@redhat.com>
2017-09-25 13:22:40 -0400

    added an else statement to find the right path to hawkular-metrics.password

--- scripts/monitoring/cron-send-metrics-checks.py
+++ scripts/monitoring/cron-send-metrics-checks.py
@@ -133,7 +133,10 @@ class OpenshiftMetricsStatus(object):
             self.hawkular_username = base64.b64decode(secret['data']['hawkular-username']).rstrip('\n')
             self.hawkular_password = base64.b64decode(secret['data']['hawkular-password']).rstrip('\n')
         except:
+            passwd_file = "/hawkular-account/hawkular-metrics.password"
+        else:
             passwd_file = "/client-secrets/hawkular-metrics.password"
+        finally:
             self.hawkular_username = 'hawkular'
             self.hawkular_password = self.oc.run_user_cmd("rsh {} cat {}".format(self.hawkular_pod_name, passwd_file))
             self.hawkular_password = self.hawkular_password.rstrip('\n')
