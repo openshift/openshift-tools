cd876a47271cffe46c0ef74274fcce44abecaecd
Drew Anderson <dranders@redhat.com>
2017-09-28 12:48:30 +1000

    Script re-write * remove test code * use logger * use library functions OCUtil * check Deployment Config * show caught errors from curl function * verbosity levels * rename smal to saml

--- scripts/monitoring/cron-send-saml-status.py
+++ scripts/monitoring/cron-send-saml-status.py
@@ -1,263 +1,130 @@
 #!/usr/bin/env python
-'''
-    Quick and dirty create application check for v3
-'''
+""" SAML Pod Check for OpenShift V3 """
 
 # Adding the ignore because it does not like the naming of the script
 # to be different than the class name
 # pylint: disable=invalid-name
 
-import subprocess
-import json
-import time
-import re
-import urllib2
-import os
-import atexit
-import shutil
-import string
-import random
+# pylint: disable=wrong-import-position
+# pylint: disable=broad-except
+
 import argparse
+import time
+import urllib2
+
+import logging
+logging.basicConfig(
+    format='%(asctime)s - %(relativeCreated)6d - %(levelname)-8s - %(message)s',
+)
+logger = logging.getLogger()
+logger.setLevel(logging.WARN)
 
 # Our jenkins server does not include these rpms.
 # In the future we might move this to a container where these
 # libs might exist
 #pylint: disable=import-error
+
+from openshift_tools.monitoring.ocutil import OCUtil
 from openshift_tools.monitoring.metric_sender import MetricSender
 
-# pylint: disable=bare-except
-def cleanup_file(inc_file):
-    ''' clean up '''
-    try:
-        os.unlink(inc_file)
-    except:
-        pass
+ocutil = OCUtil()
 
-def copy_kubeconfig(config):
-    ''' make a copy of the kubeconfig '''
-    file_name = os.path.join('/tmp',
-                             ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(7)))
-    shutil.copy(config, file_name)
-    atexit.register(cleanup_file, file_name)
+def runOCcmd(cmd, base_cmd='oc'):
+    """ log commands through ocutil """
+    logger.info(base_cmd + " " + cmd)
+    oc_time = time.time()
+    oc_result = ocutil.run_user_cmd(cmd, base_cmd=base_cmd, )
+    logger.info("oc command took %s seconds", str(time.time() - oc_time))
+    return oc_result
 
-    return file_name
-
-class OpenShiftOC(object):
-    ''' Class to wrap the oc command line tools '''
-    def __init__(self, namespace, kubeconfig, args, verbose=False):
-        ''' Constructor for OpenShiftOC '''
-        self.namespace = namespace
-        self.verbose = verbose
-        self.kubeconfig = kubeconfig
-        self.args = args
-
-    def get_pod(self):
-        '''return a pod by name'''
-        pods = self.get_pods()
-        podname = pod_name(self.args.name)
-        # this will match on build pods but not deploy pods
-        regex = re.compile('%s-[0-9]-[a-z0-9]{5}$' % podname)
-        for pod in pods['items']:
-            results = regex.search(pod['metadata']['name'])
-            if results:
-                # only report on running build pods, we'll wait if build pod is pending
-                if "build" in pod['metadata']['name'] and pod['status']['phase'] != 'Running':
-                    continue
-                else: return pod
-
-    def get_pods(self):
-        '''return all pods '''
-        cmd = ['get', 'pods', '--no-headers', '-o', 'json', '-n', self.namespace]
-        results = self.oc_cmd(cmd)
-
-        return json.loads(results)
-
-    def new_app(self, path):
-        '''run new-app '''
-        cmd = ['new-app', path, '-n', self.namespace]
-        return self.oc_cmd(cmd)
-
-    def delete_project(self):
-        '''delete project '''
-        cmd = ['delete', 'project', self.namespace]
-        results = self.oc_cmd(cmd)
-        time.sleep(5)
-        return results
-
-    def new_project(self):
-        '''create new project '''
-        version = self.get_version()
-        cmd = ['new-project', self.namespace]
-        if "v3.2" in version:  ## remove this after BZ1333049 resolved in OSE
-            rval = self.oadm_cmd(cmd)
-        else:
-            rval = self.oc_cmd(cmd)
-        return rval
-
-    def get_logs(self):
-        '''get all events'''
-        pod = self.get_pod()
-        if pod:
-            return self.oc_cmd(['logs', pod['metadata']['name'], '-n', self.namespace])
-
-        return 'Could not get logs for pod.  Could not determine pod name.'
-
-    def get_route(self):
-        '''get route to check if app is running'''
-        cmd = ['get', 'route', '--no-headers', '-o', 'json', '-n', self.namespace]
-        results = self.oc_cmd(cmd)
-        return json.loads(results)
-
-    def get_service(self):
-        '''get service to check if app is running'''
-        return json.loads(self.oc_cmd(['get', 'service', '--no-headers', '-n', 'default', '-o', 'json']))
-
-    def get_events(self):
-        '''get all events'''
-        return self.oc_cmd(['get', 'events', '-n', self.namespace])
-
-
-    def get_projects(self):
-        '''get all projects '''
-        cmd = ['get', 'projects', '--no-headers']
-        results = [proj.split()[0] for proj in self.oc_cmd(cmd).split('\n') if proj and len(proj) > 0]
-
-        return results
-
-    def get_version(self):
-        '''get openshift version'''
-        return self.oc_cmd(['version'])
-
-    def oc_cmd(self, cmd):
-        '''Base command for oc '''
-        cmds = ['/usr/bin/oc']
-        cmds.extend(cmd)
-        print ' '.join(cmds)
-        proc = subprocess.Popen(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE, \
-                                env={'KUBECONFIG': self.kubeconfig, \
-                                     'PATH': os.environ["PATH"]})
-        proc.wait()
-        if proc.returncode == 0:
-            output = proc.stdout.read()
-            if self.verbose:
-                print "Stdout:"
-                print output
-                print "Stderr:"
-                print proc.stderr.read()
-            return output
-
-        return "Error: %s.  Return: %s" % (proc.returncode, proc.stderr.read())
-
-    def oadm_cmd(self, cmd):
-        '''Base command for oadm '''
-        cmds = ['/usr/bin/oadm']
-        cmds.extend(cmd)
-        print ' '.join(cmds)
-        proc = subprocess.Popen(cmds, stdout=subprocess.PIPE, stderr=subprocess.PIPE, \
-                                env={'KUBECONFIG': self.kubeconfig, \
-                                     'PATH': os.environ["PATH"]})
-        proc.wait()
-        if proc.returncode == 0:
-            output = proc.stdout.read()
-            if self.verbose:
-                print "Stdout:"
-                print output
-                print "Stderr:"
-                print proc.stderr.read()
-            return output
-
-        return "Error: %s.  Return: %s" % (proc.returncode, proc.stderr.read())
-
-
-def curl(ip_addr, port):
-    ''' Open an http connection to the url and read
-    '''
-    code = 0
-    timeout = 30 ## only wait this number of seconds for a response
-    try:
-        code = urllib2.urlopen( \
-            'https://%s:%s/logged_out.html' % (ip_addr, port), timeout=timeout).getcode()
-    except urllib2.HTTPError, e:
-        code = e.fp.getcode()
-    except urllib2.URLError, e:
-        print "timed out in %s seconds opening http://%s:%s" % \
-            (timeout, ip_addr, port)
-    return code
-
-
+def runOCcmd_yaml(cmd, base_cmd='oc'):
+    """ log commands through ocutil """
+    logger.info(base_cmd + " " + cmd)
+    ocy_time = time.time()
+    ocy_result = ocutil.run_user_cmd_yaml(cmd, base_cmd=base_cmd, )
+    logger.info("oc command took %s seconds", str(time.time() - ocy_time))
+    return ocy_result
 
 def parse_args():
     """ parse the args from the cli """
-
     parser = argparse.ArgumentParser(description='OpenShift app create end-to-end test')
-    parser.add_argument('-v', '--verbose', action='store_true', default=None, help='Verbose?')
-    parser.add_argument('--debug', action='store_true', default=None, help='Debug?')
-    parser.add_argument('--name', default="saml-auth", help='service name')
-    return parser.parse_args()
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help='verbosity level, specify multiple')
+    parser.add_argument('--dc', default="saml-auth", help='deployment config name')
+    parser.add_argument('--service', default="saml-auth", help='service name')
+    parser.add_argument('--namespace', default="default", help='service namespace')
 
-def pod_name(name):
-    """ strip pre/suffices from app name to get pod name """
-    # for app deploy "https://github.com/openshift/hello-openshift:latest", pod name is hello-openshift
-    if 'http' in name:
-        name = name.rsplit('/')[-1]
-    if 'git' in name:
-        name = name.replace('.git', '')
-    if '/' in name:
-        name = name.split('/')[1]
-    if ':' in name:
-        name = name.split(':')[0]
-    return name
+    args = parser.parse_args()
 
+    if args.verbose > 0:
+        logger.setLevel(logging.INFO)
+    if args.verbose > 1:
+        logger.setLevel(logging.DEBUG)
 
-def send_zagg_data(key_zabbix, result):
-    ''' send data to Zagg'''
-    mts = MetricSender()
-    mts.add_metric({key_zabbix: result})
-    mts.send_metrics()
+    return args
 
-def handle_fail(run_time, oocmd, pod):
-    ''' Print failure info '''
-    print 'Finished.'
-    print 'State: Fail'
-    print 'Time: %s' % run_time
-    print 'Fetching Events:'
-    oocmd.verbose = True
-    print oocmd.get_events()
-    print 'Fetching Logs:'
-    print oocmd.get_logs()
-    print 'Fetching Pod:'
-    print pod
+def gen_test_url(service):
+    """ Generate Test URL """
+    return "https://%s:%s/logged_out.html" % (
+        service['spec']['clusterIP'],
+        443
+    )
+
+def curl(url):
+    """ Test URL, return http code """
+    try:
+        return urllib2.urlopen(url, timeout=30).getcode()
+    except urllib2.HTTPError as e:
+        return e.fp.getcode()
+    except Exception:
+        logger.exception("unknown error")
+
+    return 0
+
+def test_saml_pod(args=None, ):
+    """ Test SAML Pod Status """
+    ocutil.namespace = args.namespace
+    logger.info('Namespace: %s', args.namespace)
+
+    dc = runOCcmd_yaml("get dc {}".format(args.service))
+    logger.info('Deployment Config replicas: %s', dc['status']['replicas'])
+    logger.info('Deployment Config availableReplicas: %s', dc['status']['availableReplicas'])
+    logger.debug(dc)
+
+    if dc['status']['availableReplicas'] < 1:
+        # Pod not running
+        return 0
+
+    service = runOCcmd_yaml("get service {}".format(args.service))
+    logger.info('Service: %s', args.service)
+    logger.debug(service)
+
+    url = gen_test_url(service)
+    logger.info('Service IP: %s', service['spec']['clusterIP'])
+    logger.info('Service URL: %s', url)
+
+    curl_status = curl(url)
+    logger.info("HTTP response (curl): %s", curl_status)
+
+    if curl_status == 200:
+        # everything is ok
+        return 1
+    else:
+        # we have the pod, but bad http response
+        return 2
 
 def main():
-    ''' Do the application creation
-    '''
-    print '################################################################################'
-    print '  Starting Check the status of SAML'
-    print '################################################################################'
-    result = 0
-    key_zabbix = 'openshift.master.smal.status'
-    kubeconfig = copy_kubeconfig('/tmp/admin.kubeconfig')
+    """ SAML Pod Status """
     args = parse_args()
-    #namespace = 'ops-' + pod_name(args.name) + '-' + os.environ['ZAGG_CLIENT_HOSTNAME']
-    namespace = 'ops-zhizhangtestforservic3'
-    oocmd = OpenShiftOC(namespace, kubeconfig, args, verbose=False)
-    service_name = args.name
-    services = oocmd.get_service()
-    for ser in services['items']:
-        print 'start finding the saml pod'
-        if ser['metadata']['name'] == service_name:
-            print 'found the service', service_name
-            print 'ip of the service is :', ser['spec']['clusterIP']
-            status = curl(ser['spec']['clusterIP'], 443)
-            print 'status of ip is', status
-            if status == 200:
-                # if the value is 1 ,everything is ok
-                result = 1
-            else:
-                # if the value =2 ,shows that we have the pod ,but the pod is in bad healthz
-                result = 2
+    logger.debug("args: ")
+    logger.debug(args)
+
+    result = test_saml_pod(args=args, )
+
     #send the value to zabbix
-    send_zagg_data(key_zabbix, result)
+    mts = MetricSender(verbose=args.verbose)
+    mts.add_metric({'openshift.master.saml.status': result})
+    mts.send_metrics()
 
 if __name__ == "__main__":
     main()
