5ef345b1e58f8d923c4f896126774210a62edcdc
Matt Woodson <mwoodson@redhat.com>
2017-10-26 09:51:18 -0400

    zabbix server partitioning bits

--- docker/oso-zabbix-server/centos7/start.sh
+++ docker/oso-zabbix-server/centos7/start.sh
@@ -18,9 +18,15 @@ echo user:x:$(id -u):0:USER:/root:/bin/bash >> /etc/passwd
 ansible-playbook /root/config.yml
 
 echo
-echo 'Ensure database exists.'
+echo 'Ensure database exist.'
 echo '---------------'
 /root/zabbix/createdb.sh
+echo
+
+echo
+echo 'Ensure zabbix db paritioning procedures exist.'
+echo '---------------'
+/root/zabbix/create_db_partitioning_procedures.sh
 echo
 
 echo
@@ -35,6 +41,17 @@ echo '---------------'
 /usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
 echo
 
+echo
+echo 'Starting zabbix partition maintenance & monitoring'
+echo '---------------'
+/usr/local/bin/zabbix_partition_maintenance.sh &
+
+# Need to sleep so the partitions are created before they are monitored
+sleep 120
+/usr/local/bin/zabbix_partition_monitoring.sh &
+echo
+
+set +e
 while true; do
   sleep 15
   pgrep -f "/usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf" &>/dev/null
@@ -42,5 +59,12 @@ while true; do
     then
       exit 1
   fi
+
+  for prog in /usr/local/bin/zabbix_partition_maintenance.sh /usr/local/bin/zabbix_partition_monitoring.sh; do
+    pgrep -f "/bin/sh ${prog}" &>/dev/null
+    if [ "$?" -eq "1" ]
+      then
+        ${prog} &
+    fi
+  done
 done
-#
