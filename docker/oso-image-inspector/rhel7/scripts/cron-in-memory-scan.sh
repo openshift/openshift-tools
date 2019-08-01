4a20b0c09a4552a2b0fba4556066c28b57586a5c
Matthew Barnes <mbarnes@fedoraproject.org>
2019-08-01 19:03:56 +0000

    docker: Don't keep generated code in source control

--- docker/oso-image-inspector/rhel7/scripts/cron-in-memory-scan.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
-chroot /host /usr/local/bin/scanpod-inmem >> /var/log/clam/in-memory-scan.log
--- docker/oso-zaio/rhel7/zabbix/db_create/createdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-mysql -uroot < create_zabbix.sql
-
-DBCREATEFILE=$(rpm -ql zabbix-server-mysql | grep 'create.sql')
-zcat $DBCREATEFILE | /usr/bin/mysql -uroot zabbix
